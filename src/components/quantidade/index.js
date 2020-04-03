dec38686e1bb1cafcfebeb1257e7f20ab9629e42
mariliafranco <marilia.andrad@gmail.com>
2020-04-03 15:04:41 -0300

    tabela estilizada

---
+++ src/components/quantidade/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Quantidade extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { idUser: props.idUser, classe: props.classe, totalItens: 0 }
+    }
+
+    componentDidMount = () => {
+        axios.get(`https://jsonplaceholder.typicode.com/users/${this.state.idUser}/${this.state.classe}`)
+            .then(resposta => {
+                this.setState({ totalItens: resposta.data.length });
+            });
+    };
+
+    render() {
+        return (
+            <td>
+                {this.state.totalItens}
+            </td>
+        )
+    }
+}
---
+++ src/components/userList/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import axios from "axios"
+import Quantidade from "../quantidade"
+
+export default class UserList extends Component {
+  state = {
+    user: []
+  };
+
+  componentDidMount = () => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(resposta => {
+        this.setState({ user: resposta.data });
+      });
+  };
+
+  render() {
+    return (
+      <tbody>
+        {this.state.user.map(items => (
+          <tr key={items.id}>
+            <td>{items.name}</td>
+            <td>{items.email}</td>
+            <td>{items.phone}</td>
+            <Quantidade idUser={items.id} classe="albums" />
+            <Quantidade idUser={items.id} classe="posts" />
+            <Quantidade idUser={items.id} classe="photos" />
+          </tr>
+        ))}
+      </tbody>
+    )
+  }
+}
